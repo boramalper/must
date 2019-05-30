32f00be8e7f99590203ddcae3ab3a85b8f02fe9d
Bora M. Alper <bora@boramalper.org>
2019-05-30 13:21:22 +0100

    add godoc

--- init.go
+++ init.go
@@ -1,8 +1,9 @@
-// Copyright (c) 2019 Mert Bora ALPER <bora@boramalper.org>
-// See LICENSE for details.
-
+// Package must provides utility functions to handle unrecoverable errors in
+// Go.
 package must
 
+// Context provides all the necessary information to handle errors. Currently
+// only `handler` is stored.
 type Context struct {
 	handler func(error)
 }
@@ -29,16 +30,23 @@ func MVV(v interface{}, e error) { defaultContext.MustValVoid(v, e) }
 
 func SetHandler(hnd func (error)) { defaultContext.SetHandler(hnd) }
 
-// ------
+func New(handler func(error)) Context {
+	return Context{
+		handler: handler,
+	}
+}
 
+// Handles if err != nil.
 func (mc Context) Must(err error) {
 	if err != nil {
 		mc.handle(err)
 	}
 }
 
+// Shorthand for Must.
 func (mc Context) M(e error) { mc.Must(e) }
 
+// Handles if err != nil, returns val otherwise.
 func (mc Context) MustVal(val interface{}, err error) interface{} {
 	if err != nil {
 		mc.handle(err)
@@ -48,14 +56,18 @@ func (mc Context) MustVal(val interface{}, err error) interface{} {
 	return val
 }
 
+// Shorthand for MustVal.
 func (mc Context) MV(v interface{}, e error) interface{} { return mc.MustVal(v, e) }
 
+// Handles if err != nil and ignores val.
 func (mc Context) MustValVoid(val interface{}, err error) {
 	mc.MustVal(val, err)
 }
 
+// Shorthand for MustValVoid.
 func (mc Context) MVV(v interface{}, e error) { mc.MustValVoid(v, e) }
 
+// Changes the handler function.
 func (mc Context) SetHandler(hnd func (error)) {
 	mc.handler = hnd
 }
@@ -67,4 +79,3 @@ func (mc Context) handle(err error) {
 		mc.handler(err)
 	}
 }
-
